1c77d01d2aa8f69ae9ad81a856323047d7822524
Micah Yeager <myeager@innovativeinc.net>
2024-06-14 16:12:46 -0400

    refactor: remove Jest options that aren't specifically needed for this

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 /** @type {import('jest').Config} */
 const config = {
-  // Since these are integration tests with a relatively slow API, increase the timeout to 20s.
-  testTimeout: 20_000,
   clearMocks: true,
-  // Increase timeout from the default of 1s to 5s to allow time for fetch's TCPWRAP and TLSWRAP handles to close.
-  openHandlesTimeout: 5_000
 };
 
 module.exports = config;
