04026bc6393f99a4f12810967659adfa3a456942
Micah Yeager <myeager@innovativeinc.net>
2024-06-14 08:37:13 -0400

    feat: implement automatic retries

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout: 20_000,
+    clearMocks: true,
     typecheck: {
       // Leave disabled by default, will be enabled as-needed by the test runner.
       // When type-checking, this specifies to ONLY check types. This works around some issues with @apigrate's code.
